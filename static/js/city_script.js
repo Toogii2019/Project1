06b2a8ec4741aec28a4733f012415e0a02869946
Toogii <dashdava@uw.edu>
2020-07-25 16:35:01 -0700

    Added trip duration

--- static/js/city_script.js
+++ static/js/city_script.js
@@ -26,16 +26,19 @@ function calcRoute(directionsService, directionsDisplay) {
         destination: localStorage.getItem("destination-town"),
         travelMode: 'DRIVING'
     },function(response, status) {
+        console.log(response);
         if (status === "OK") {
             directionsDisplay.setDirections(response);
         }
         else {
             window.alert('Direction request has failed!! - ' + status);
         }
-        $("#source").text(response.request.origin.query);
-        $("#destination").text(response.request.destination.query);
+        $("#source").text(response.routes[0].legs[0].start_address);
+        $("#destination").text(response.routes[0].legs[0].end_address);
 
         $("#distance").text(response.routes[0].legs[0].distance.text);
+        $("#duration").text(response.routes[0].legs[0].duration.text);
+
         $("#travel-mode").text(response.request.travelMode);
 
     }   
