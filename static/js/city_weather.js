427fc5b6f5f540e040ae1a0ffdf3b4fbe07d563d
Toogii <dashdava@uw.edu>
2020-07-26 11:20:59 -0700

    Added line to call getWeather(), response.data changed to response.list

--- static/js/city_weather.js
+++ static/js/city_weather.js
@@ -1,6 +1,6 @@
 var APIKey = "4329b9a304464e3aaf6df7df53ecd8b3";
 let cityName = localStorage.getItem("destination-town"); 
-
+getWeather();
 function getWeather() {
 
     //  Using saved city name, execute a 5-day forecast get request
@@ -17,15 +17,15 @@ function getWeather() {
             console.log(forecastQueryURL);
             console.log(response);
 
-            //Get Date
-            //const currentDate = new Date(response.data.dt*1000);
+            // Get Date
+            const currentDate = new Date(response.dt*1000);
             console.log(currentDate);
 
             const forecast = document.querySelectorAll(".forecast");
             for (i=0; i<forecast.length; i++) {
                 forecast[i].innerHTML = "";
                 const forecastIndex = i*8 + 4;
-                const forecastDate = new Date(response.data.list[forecastIndex].dt * 1000);
+                const forecastDate = new Date(response.list[forecastIndex].dt * 1000);
                 const forecastDay = forecastDate.getDate();
                 const forecastMonth = forecastDate.getMonth() + 1;
                 const forecastYear = forecastDate.getFullYear();
@@ -34,14 +34,14 @@ function getWeather() {
                 forecastDateP.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
                 forecast[i].append(forecastDateP);
                 const forecastWeather = document.createElement("img");
-                forecastWeather.setAttribute("src","https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
-                forecastWeather.setAttribute("alt",response.data.list[forecastIndex].weather[0].description);
+                forecastWeather.setAttribute("src","https://openweathermap.org/img/wn/" + response.list[forecastIndex].weather[0].icon + "@2x.png");
+                forecastWeather.setAttribute("alt",response.list[forecastIndex].weather[0].description);
                 forecast[i].append(forecastWeather);
                 const forecastTemp = document.createElement("p");
-                forecastTemp.innerHTML = "Temp: " + response.data.list[forecastIndex].main.temp + " ºF";
+                forecastTemp.innerHTML = "Temp: " + response.list[forecastIndex].main.temp + " ºF";
                 forecast[i].append(forecastTemp);
                 const forecastHumidity = document.createElement("p");
-                forecastHumidity.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
+                forecastHumidity.innerHTML = "Humidity: " + response.list[forecastIndex].main.humidity + "%";
                 forecast[i].append(forecastHumidity);
                 
             }
